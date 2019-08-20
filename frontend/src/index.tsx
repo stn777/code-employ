600133708bc1fe7399051f1516ac89023cd5da05
Aden Herold <aden.herold1@gmail.com>
2019-08-20 18:59:53 +0930

    added frontend project, built from react + typescript boilerplate

---
+++ frontend/src/index.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Route, HashRouter as Router, Switch, Link } from 'react-router-dom';
+
+import { Hello } from "./components/Hello";
+
+class App extends React.Component {
+    render() {
+      return (
+        <Router>
+          <div>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/Two">Two</Link>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={Hello} />
+              <Route exact path="/Two" component={Hello} />
+            </Switch>
+          </div>
+        </Router>
+      );
+    }
+  }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
