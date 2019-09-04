c1ce236acba1fc631e94d7586a68f2f5f04461fd
Aden Herold <48753915+adenh93@users.noreply.github.com>
2019-09-04 09:57:25 +0930

    remove unnecessary global declaration from frontend/src/store/configureStore.ts

--- frontend/src/store/configureStore.ts
+++ frontend/src/store/configureStore.ts
@@ -3,12 +3,6 @@ import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-declare global {
-  interface Window {
-    redux_dev_tools: any;
-  }
-}
-
 export default function configureStore() {
   const composeEnhancers =
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
